3eab1a8797498de0a94fe308d73e86130d0c504d
yumapiero <g1527046@st.t-kougei.ac.jp>
2016-06-20 17:18:24 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,4 +1,4 @@
-// 答えを用意
+/*答え*/
 var number = Math.floor(Math.random()*3);
 var answer = parseInt(window.prompt('数あてゲーム。0～2の数字を入力してください！'));
 
@@ -6,12 +6,12 @@ var message;
 if(answer === number) {
   message = 'あたり！';
 }else if (answer < number){
-  message = '残念！もっと大きいです';
+  message = '残念！もっと大きいです!';
 }else if (number < answer){
-  message = '残念！もっと小さいです';
+  message = '残念！もっと小さいです!';
 }else{
   message = '0～3の数字を入力してください';
 }
 
-// 結果を表示
+/*結果*/
 document.getElementById('choice').textContent = message;
